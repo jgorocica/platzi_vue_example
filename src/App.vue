b6a8040954dfa1c8f256e1676aabe7c7b2a8ae86
Julio Hernández <eduar881@gmail.com>
2019-04-02 22:24:03 -0500

    Fix url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://jgorocica.github.io/platzi_vue_example/dist/logo.png')
     h1 {{ msg }}
     select(v-model="selectedCountry")
       option(v-for="(country, index) in countries" :key="index" :value="country.value") {{country.name}}
